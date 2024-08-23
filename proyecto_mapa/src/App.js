712f019882cff38055371a5b45fcb7b2e06bf0ce
Álvaro <alvarocc948@gmail.com>
2024-08-23 14:31:32 +0200

    nuevo css

--- proyecto_mapa/src/App.js
+++ proyecto_mapa/src/App.js
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import './App.css';
 import { Tooltip } from 'react-tooltip'; 
 
-
-
 import {ComposableMap, Geographies, Geography, Marker, Annotation, ZoomableGroup } from "react-simple-maps";
 //import 'react-tooltip/dist/react-tooltip.css'
 
@@ -17,55 +15,79 @@ const marcadores = [
 
 function App() {
   const [contenido, setcontenido] = useState("");
-  return (
-    <div className="App">
-      <div className="title">
-        <h1>World Map</h1>
-      </div>
-      
-      <div className="columna1">
-          <p>lorem  </p>
-      </div>
-      
-      <Tooltip id='country-tooltip'/>
+  const [paisSeleccionado, setPaisSeleccionado] = useState(null);
 
-      <div className="marco">
-      <ComposableMap className="map" data-tip="">
-        <ZoomableGroup>
-          <Geographies geography="/features.json">
-            {({ geographies }) =>
-                geographies.map((geo) => (
-                  <Geography key={geo.rsmKey} geography={geo}
-                  data-tooltip-id="country-tooltip"
-                  data-tooltip-content={geo.properties.name} 
-                  onMouseEnter={() => {
-                    const{NAME} = geo.properties;
-                    setcontenido(`${NAME}`);
-                  }}
-                  onMouseLeave={() => {setcontenido("");}}
-                  style={{
-                    hover: {
-                      fill: "#62e55f",
-                      outline: "none",
-                    }
-                  }}
-                  />
-                ))
-              }
-          </Geographies>     
-                {marcadores.map(({ name, coordinates, markerOffset}) => (
-                  <Marker key ={name} coordinates={coordinates} >
-                    <circle r={3} fill='#F00' stroke='#fff' strokeWidth={1}/>
-                    <text textAnchor='middle' y={markerOffset} style={{fontFamily: "system-ui", fill: "#06a7fe"}}>
-                      {name}
-                    </text>
-                  </Marker>
-                ))}
-        </ZoomableGroup>
-        
-      </ComposableMap>
-        
-      </div> 
+  return (
+    
+    <div className="App">
+        <header class="header">
+          <h1>World Map</h1>
+        </header>
+      <Tooltip id='country-tooltip'/>
+      <div className="section">
+        <div className="card">
+          <h1>Información del pais</h1>
+          <hr></hr>
+          <div className="container">
+          {paisSeleccionado ? (
+              <div>
+                <h2>{paisSeleccionado.name}</h2>
+                <p>Capital: {paisSeleccionado.capital}</p>
+                <p>Población: {paisSeleccionado.population}</p>
+                <p>Región: {paisSeleccionado.region}</p>
+              </div>
+            ) : (
+              <p>Haz clic en un país para ver más información.</p>
+            )}
+          </div>
+        </div>
+        <div className="marco">
+        <ComposableMap className="map" data-tip="">
+          <ZoomableGroup>
+            <Geographies geography="/features.json">
+              {({ geographies }) =>
+                  geographies.map((geo) => (
+                    <Geography key={geo.rsmKey} geography={geo}
+                    data-tooltip-id="country-tooltip"
+                    data-tooltip-content={geo.properties.name}
+                    onClick={() => {
+                      const { NAME, CAPITAL, POP_EST, REGION_UN } = geo.properties;
+                      setPaisSeleccionado({
+                        name: NAME,
+                        capital: CAPITAL || 'Sin datos',
+                        population: POP_EST,
+                        region: REGION_UN,
+                      });
+                    }}
+                     
+                    onMouseEnter={() => {
+                      const{NAME} = geo.properties;
+                      setcontenido(`${NAME}`);
+                    }}
+                    onMouseLeave={() => {setcontenido("");}}
+                    style={{
+                      hover: {
+                        fill: "#6363db",
+                        outline: "none",
+                      }
+                    }}/>
+                  ))
+                }
+            </Geographies>     
+                  {marcadores.map(({ name, coordinates, markerOffset}) => (
+                    <Marker key ={name} coordinates={coordinates} >
+                      <circle r={3} fill='#F00' stroke='#fff' strokeWidth={1}/>
+                      <text textAnchor='middle' y={markerOffset} style={{fontFamily: "system-ui", fill: "#06a7fe"}}>
+                        {name}
+                      </text>
+                    </Marker>
+                  ))}
+          </ZoomableGroup>
+          
+        </ComposableMap>
+
+        </div> 
+      </div>
       <div className="texto">
           <p>Gracias por visitar este proyecto realizado con ReactJS, si quieres ver otros proyectos en los que he participado haz click en el botón de abajo.</p>
           <a href="https://github.com/iToxicD">Saber más</a>
